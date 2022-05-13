ae1dc662dba54e50b5d5fa97a99efd25d7a935d3
sandypockets <zac@znacreative.com>
2022-05-12 20:56:45 -0400

    Add source link to nav

--- components/Layout/Nav.jsx
+++ components/Layout/Nav.jsx
@@ -2,12 +2,9 @@ import Link from 'next/link';
 
 const navItems = [
   {
-    name: 'Home',
-    href: '/',
-  },
-  {
-    name: 'About',
-    href: '/about',
+    name: 'View source on GitHub',
+    href: 'https://github.com/sandypockets/descriptify',
+    target: '_blank',
   },
 ];
 
@@ -18,7 +15,10 @@ export default function Nav() {
         {navItems.map((item, index) => (
           <li key={index} className="mt-7">
             <Link href={item.href}>
-              <a className="p-2 mx-2 hover:text-transparent bg-clip-text bg-gradient-to-r from-green-300 to-blue-500 transition-colors duration-200">
+              <a
+                target={item.target}
+                className="p-2 mx-2 hover:text-transparent bg-clip-text bg-gradient-to-r from-green-300 to-blue-500 transition-colors duration-200"
+              >
                 {item.name}
               </a>
             </Link>
--- pages/about.jsx
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/Layout/Layout';
-
-export default function About() {
-  return (
-    <Layout>
-      <h1>About page</h1>
-    </Layout>
-  );
-}
